80d721fa648086bcd93fa82147de459da0197f07
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-21 16:45:57 +0430

    const class for database tables created and information for users table added

---
+++ src/main/java/com/example/todo_app/database/Const.java
@@ -0,0 +1,15 @@
+package com.example.todo_app.database;
+
+public class Const {
+    final static String USERS_TABLE = "users";
+
+    //Users Table Column Names
+    final static String UERS_ID = "idusers";
+    final static String USER_FIRSTNAME = "firstname";
+    final static String USER_LASTNAME = "lastname";
+    final static String USER_ADDRESS = "address";
+    final static String USER_GENDER = "gender";
+    final static String USER_USERNAME = "username";
+    final static String USER_PASSWORD = "password";
+
+}
