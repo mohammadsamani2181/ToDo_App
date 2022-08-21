ac8cb9b0e800aca3545ad68aaee3869e710fea75
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-21 21:53:09 +0430

    saving user's information added

--- src/main/java/com/example/todo_app/SignUpPageController.java
+++ src/main/java/com/example/todo_app/SignUpPageController.java
@@ -1,12 +1,16 @@
 package com.example.todo_app;
 
+import com.example.todo_app.database.DBHandler;
+import com.example.todo_app.model.User;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXCheckbox;
 import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
+import java.sql.SQLException;
 
 public class SignUpPageController {
+    private DBHandler dbHandler;
 
     @FXML
     private MFXTextField signUpPageFirstnameFld;
@@ -34,6 +38,40 @@ public class SignUpPageController {
 
     @FXML
     void initialize() {
+        signUpPageSignupBtn.setOnAction(e -> {
+            String firstname = signUpPageFirstnameFld.getText().trim();
+            String lastname = signUpPageLastnameFld.getText().trim();
+            String address = signUpPageAddressFld.getText().trim();
+            String username = signUpPageUsernameFld.getText().trim();
+            String password= signUpPagePasswordFld.getText().trim();
+            String gender;
+            if (signUpPageFemaleBox.isSelected()) {
+                gender = signUpPageFemaleBox.getText().trim();
+            }else {
+                gender = signUpPageMaleBox.getText().trim();
+            }
 
+            if (!firstname.equals("") && !lastname.equals("")
+                    && !address.equals("") && !username.equals("")
+                    && !password.equals("")) {
+
+                User newUser = new User(firstname, lastname,
+                                        username, password,
+                                        address, gender);
+
+                dbHandler = new DBHandler();
+                try {
+                    dbHandler.createNewUser(newUser);
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+            }else {
+
+            }
+
+        });
     }
 }
