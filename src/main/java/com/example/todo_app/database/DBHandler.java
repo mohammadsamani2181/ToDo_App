363713ded95e4ddf944c8a01bf7a7eadf6088f95
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-21 21:55:07 +0430

    createNewUser method created for adding user's information to the users table

--- src/main/java/com/example/todo_app/database/DBHandler.java
+++ src/main/java/com/example/todo_app/database/DBHandler.java
@@ -1,6 +1,9 @@
 package com.example.todo_app.database;
+import com.example.todo_app.model.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBHandler extends Configs{
@@ -13,8 +16,24 @@ public class DBHandler extends Configs{
 
         Class.forName("com.mysql.jdbc.Driver");
 
-        dbConnection = DriverManager.getConnection(connectionString + dbUser + dbPassword);
+        dbConnection = DriverManager.getConnection(connectionString , dbUser, dbPassword);
 
         return dbConnection;
+    }
+
+    public void createNewUser (User user) throws SQLException, ClassNotFoundException {
+        String insert = "INSERT INTO users(firstname, lastname, address, gender, password, username)" +
+                "VALUES(?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement preparedStatement = getDbConnection().prepareStatement(insert);
+
+        preparedStatement.setString(1, user.getFirstname());
+        preparedStatement.setString(2, user.getLastname());
+        preparedStatement.setString(3, user.getAddress());
+        preparedStatement.setString(4, user.getGender());
+        preparedStatement.setString(5, user.getPassword());
+        preparedStatement.setString(6, user.getUsername());
+
+        preparedStatement.executeUpdate();
     }
 }
