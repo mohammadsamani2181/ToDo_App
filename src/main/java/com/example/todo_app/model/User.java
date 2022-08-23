0510d79f199766336229ced03b1501432e7e59b0
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-23 16:54:34 +0430

    some setters method and a new field (id) added

--- src/main/java/com/example/todo_app/model/User.java
+++ src/main/java/com/example/todo_app/model/User.java
@@ -1,6 +1,7 @@
 package com.example.todo_app.model;
 
 public class User {
+    private int id;
     private String firstname;
     private String lastname;
     private String username;
@@ -24,23 +25,55 @@ public class User {
         return firstname;
     }
 
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
     public String getLastname() {
         return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getAddress() {
         return address;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public String getGender() {
         return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
