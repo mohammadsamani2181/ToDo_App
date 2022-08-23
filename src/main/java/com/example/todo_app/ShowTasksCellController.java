f66127499fd045c83019dfaa3dcd96338926c350
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-23 19:26:29 +0430

    showTasksCell Controller created and some ids added

---
+++ src/main/java/com/example/todo_app/ShowTasksCellController.java
@@ -0,0 +1,21 @@
+package com.example.todo_app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ShowTasksCellController {
+
+    @FXML
+    private Label tasksCellDescriptionFld;
+
+    @FXML
+    private Label tasksCellTaskFld;
+
+    @FXML
+    private Label tasksCellDateCreatedFld;
+
+    @FXML
+    void initialize() {
+
+    }
+}
