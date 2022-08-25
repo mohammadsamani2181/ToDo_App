900b40a6b6df27c3b7022db8874896e7d534ca63
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-26 01:37:06 +0430

    extending from ListCell class and updateItem method added

--- src/main/java/com/example/todo_app/ShowTasksCellController.java
+++ src/main/java/com/example/todo_app/ShowTasksCellController.java
@@ -1,9 +1,15 @@
 package com.example.todo_app;
 
+import com.example.todo_app.model.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.AnchorPane;
 
-public class ShowTasksCellController {
+import java.io.IOException;
+
+public class ShowTasksCellController extends ListCell<Task> {
 
     @FXML
     private Label tasksCellDescriptionFld;
@@ -15,7 +21,40 @@ public class ShowTasksCellController {
     private Label tasksCellDateCreatedFld;
 
     @FXML
+    private AnchorPane tasksCellRoot;
+
+    private FXMLLoader loader;
+
+    @FXML
     void initialize() {
 
+    }
+
+    @Override
+    protected void updateItem(Task task, boolean empty) {
+        super.updateItem(task, empty);
+
+        if (task == null || empty) {
+            setText(null);
+            setGraphic(null);
+        }else {
+            if (loader == null) {
+                loader = new FXMLLoader(getClass().getResource("showTasksCell.fxml"));
+                loader.setController(this);
+
+                try {
+                    loader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            tasksCellTaskFld.setText(task.getTask());
+            tasksCellDescriptionFld.setText(task.getDescription());
+            tasksCellDateCreatedFld.setText(task.getDateCreated().toString());
+
+            setText(null);
+            setGraphic(tasksCellRoot);
+        }
     }
 }
