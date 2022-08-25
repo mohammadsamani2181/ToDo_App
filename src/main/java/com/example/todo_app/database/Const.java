915ad75696fcd9ccdb1de38ed729f4905a6f266d
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-25 13:27:41 +0430

    tasks table information added

--- src/main/java/com/example/todo_app/database/Const.java
+++ src/main/java/com/example/todo_app/database/Const.java
@@ -1,10 +1,12 @@
 package com.example.todo_app.database;
 
 public class Const {
+    //Tables
     final static String USERS_TABLE = "users";
+    final static String TASKS_TABLE = "tasks";
 
     //Users Table Column Names
-    final static String UERS_ID = "idusers";
+    final static String USER_ID = "idusers";
     final static String USER_FIRSTNAME = "firstname";
     final static String USER_LASTNAME = "lastname";
     final static String USER_ADDRESS = "address";
@@ -12,4 +14,10 @@ public class Const {
     final static String USER_USERNAME = "username";
     final static String USER_PASSWORD = "password";
 
+    //Tasks Table Column Names
+    final static String TASK_ID = "idtasks";
+    final static String TASK_IDUSER = "idusers";
+    final static String TASK_TASK = "task";
+    final static String TASK_DESCRIPTION = "description";
+    final static String TASK_DATECREATED = "dateCreated";
 }
