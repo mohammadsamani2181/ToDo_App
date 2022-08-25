720eba5f4fddc1c0d72b3022f320e07ad49b6ad2
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-25 17:44:36 +0430

    adding tasks from this page added

--- src/main/java/com/example/todo_app/TasksPageController.java
+++ src/main/java/com/example/todo_app/TasksPageController.java
@@ -1,11 +1,25 @@
 package com.example.todo_app;
 
+import com.example.todo_app.database.DBHandler;
+import com.example.todo_app.model.Task;
+import com.example.todo_app.model.User;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import io.github.palexdev.materialfx.controls.legacy.MFXLegacyListView;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class TasksPageController {
+
+    private User user;
 
     @FXML
     private MFXLegacyListView<?> tasksPageListView;
@@ -17,10 +31,60 @@ public class TasksPageController {
     private MFXTextField tasksPageDescriptionFld;
 
     @FXML
-    private MFXButton tasksPageSaveBtn;
+    private MFXButton tasksPageAddBtn;
+
+    @FXML
+    private Label tasksPageSuccessfullLbl;
+
+    @FXML
+    private ImageView tasksPageImageView;
 
     @FXML
     void initialize() {
+        tasksPageAddBtn.setOnAction(e -> {
 
+            String task = tasksPageTaskFld.getText().trim();
+            String description = tasksPageDescriptionFld.getText().trim();
+            DBHandler dbHandler;
+            Task newTask;
+
+            if (!task.equals("") && !description.equals("")) {
+                dbHandler = new DBHandler();
+                newTask = new Task(user.getId(), task, description, new Timestamp(System.currentTimeMillis()));
+
+                try {
+
+                    dbHandler.addNewTask(newTask);
+
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+
+//                PauseTransition visiblePause = new PauseTransition(
+//                        Duration.seconds(3)
+//                );
+//                visiblePause.setOnFinished(
+//                        event -> tasksPageSuccessfullLbl.setVisible(true)
+//                );
+//                visiblePause.setOnFinished(
+//                          event -> tasksPageImageView
+//                );
+//                visiblePause.play();
+                
+
+                tasksPageTaskFld.setText("");
+                tasksPageDescriptionFld.setText("");
+
+            }else {
+                System.out.println("you must enter description and task!");
+            }
+        });
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
