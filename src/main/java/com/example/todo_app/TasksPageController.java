fc26c4efce128916ede659fa22811efe4d47f3d9
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-26 19:14:39 +0430

    a new method for showing an image and a label added

--- src/main/java/com/example/todo_app/TasksPageController.java
+++ src/main/java/com/example/todo_app/TasksPageController.java
@@ -6,11 +6,13 @@ import com.example.todo_app.model.User;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import io.github.palexdev.materialfx.controls.legacy.MFXLegacyListView;
+import javafx.animation.PauseTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 
 import java.sql.ResultSet;
@@ -83,20 +85,12 @@ public class TasksPageController {
 
             tasks.add(newTask);
 
-//                PauseTransition visiblePause = new PauseTransition(
-//                        Duration.seconds(3)
-//                );
-//                visiblePause.setOnFinished(
-//                        event -> tasksPageSuccessfullLbl.setVisible(true)
-//                );
-//                visiblePause.setOnFinished(
-//                          event -> tasksPageImageView
-//                );
-//                visiblePause.play();
-
-
             tasksPageTaskFld.setText("");
             tasksPageDescriptionFld.setText("");
+
+            tasksPageSuccessfullLbl.setVisible(true);
+            tasksPageImageView.setVisible(true);
+            invisibleLabelAndImage();
 
         }else {
             System.out.println("you must enter description and task!");
@@ -125,6 +119,25 @@ public class TasksPageController {
 
         tasksPageListView.setItems(tasks);
         tasksPageListView.setCellFactory(ShowTasksCellController -> new ShowTasksCellController());
+    }
+
+    private void invisibleLabelAndImage() {
+        PauseTransition visiblePause1 = new PauseTransition(
+                Duration.seconds(5)
+        );
+        visiblePause1.setOnFinished(
+                event -> tasksPageImageView.setVisible(false)
+        );
+        visiblePause1.play();
+
+
+        PauseTransition visiblePause2 = new PauseTransition(
+                Duration.seconds(5)
+        );
+        visiblePause2.setOnFinished(
+                event -> tasksPageSuccessfullLbl.setVisible(false)
+        );
+        visiblePause2.play();
     }
 
 }
