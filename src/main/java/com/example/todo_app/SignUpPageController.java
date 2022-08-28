02254334ef26688aea0327860ba8d3e8d190ded0
mmd_s21 <hdhdmohammad5@gmail.com>
2022-08-29 00:51:56 +0430

    now you can go back to the login page and a binding for check boxes added

--- src/main/java/com/example/todo_app/SignUpPageController.java
+++ src/main/java/com/example/todo_app/SignUpPageController.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -43,12 +45,66 @@ public class SignUpPageController {
     private MFXButton signUpPageSignupBtn;
 
     @FXML
+    private ImageView signUpPageBackIcon;
+
+    @FXML
+    private Label signUpPageErrorLbl;
+
+
+
+    @FXML
     void initialize() {
+
+        signUpPageMaleBox.setOnMouseClicked(e -> {
+
+            if (signUpPageMaleBox.isSelected()) {
+                signUpPageFemaleBox.setSelected(false);
+
+            }else {
+                signUpPageFemaleBox.setSelected(true);
+            }
+
+        });
+
+        signUpPageFemaleBox.setOnMouseClicked(e -> {
+
+            if (signUpPageFemaleBox.isSelected()) {
+                signUpPageMaleBox.setSelected(false);
+
+            }else {
+                signUpPageMaleBox.setSelected(true);
+            }
+
+        });
+
         signUpPageSignupBtn.setOnAction(e -> {
 
             signUpUser();
 
         });
+
+        signUpPageBackIcon.setOnMouseClicked(e -> {
+
+            backToTheLoginPage();
+
+        });
+    }
+
+    private void backToTheLoginPage() {
+        signUpPageBackIcon.getScene().getWindow().hide();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("loginPage.fxml"));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     private void showLoginPage() {
@@ -101,7 +157,8 @@ public class SignUpPageController {
             showLoginPage();
 
         }else {
-            System.out.println("all the information must are entered");
+            signUpPageErrorLbl.setText("Please enter all the information.");
+            MakeInvisible.start(signUpPageErrorLbl);
         }
     }
 }
